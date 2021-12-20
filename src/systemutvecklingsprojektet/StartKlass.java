91bb77eba9871cf5d85bfcee16efa64bddb3df1a
juliusdahlstrom <juliusdahlstroem@hotmail.com>
2021-12-20 14:26:21 +0100

    Har skapat startklass typ!

---
+++ src/systemutvecklingsprojektet/StartKlass.java
@@ -0,0 +1,32 @@
+package systemutvecklingsprojektet;
+
+import javax.swing.JOptionPane;
+import oru.inf.InfDB;
+import oru.inf.InfException;
+
+/**
+ *
+ * @author alval
+ */
+public class StartKlass {
+   private static InfDB idb;
+   
+   public static void main(String[] args)
+   {
+       try
+       {
+           idb = new InfDB("mibdba", "3306", "mibdb", "mibdba");
+           
+       }
+
+        catch(InfException felUppgifter)
+                {
+                    JOptionPane.showMessageDialog(null, "något gick fel");
+                    System.out.println("Felmeddelande" + felUppgifter.getMessage());
+                }
+                new Fonster(idb).setVisible(true);
+   
+  
+    }
+    
+}
