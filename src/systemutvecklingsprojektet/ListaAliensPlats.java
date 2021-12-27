20700b90e635466c2121a2680e4dd033a89c43f1
juliusdahlstrom <juliusdahlstroem@hotmail.com>
2021-12-27 11:23:48 +0100

    Har fixat InfoOmOmrådesChef så att knappen funkar (tror jag)

--- src/systemutvecklingsprojektet/ListaAliensPlats.java
+++ src/systemutvecklingsprojektet/ListaAliensPlats.java
@@ -43,6 +43,11 @@ public class ListaAliensPlats extends javax.swing.JFrame {
         LabelVäljPlats.setText("Välj plats:");
 
         ListaVäljPlats.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+        ListaVäljPlats.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                ListaVäljPlatsActionPerformed(evt);
+            }
+        });
 
         TextAreaPlatsLista.setColumns(20);
         TextAreaPlatsLista.setRows(5);
@@ -84,6 +89,10 @@ public class ListaAliensPlats extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void ListaVäljPlatsActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ListaVäljPlatsActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_ListaVäljPlatsActionPerformed
 
     /**
      * @param args the command line arguments
